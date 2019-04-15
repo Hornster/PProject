9be9fc1e0afff6e29e37af617ddbcecfbbab9cda
Karol Kozuch <karol199714@gmail.com>
2019-04-15 19:43:56 +0200

    Add project files.

---
+++ PProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PProject.Startup))]
+namespace PProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
